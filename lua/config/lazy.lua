227cbcc138c7ee6350932e2dc322b52e9682c0cc
Andrew Brower <bigspeedfpv@gmail.com>
2025-01-03 02:30:21 -0500

    fix rust!

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -118,17 +118,9 @@ require("lazy").setup({
       require("mason-lspconfig").setup()
 
       require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
+        function (server_name) 
             require("lspconfig")[server_name].setup {}
         end,
-        -- Next, you can provide a dedicated handler for specific servers.
-        -- For example, a handler override for the `rust_analyzer`:
-        ["rust_analyzer"] = function ()
-            require("rust-tools").setup {}
-        end
       }
 
       vim.api.nvim_create_autocmd("LspAttach", {
