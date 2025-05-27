ecfe160f3d1e5b89f02359f0fcb5429e6651fab4
Andrew Brower <bigspeedfpv@gmail.com>
2025-05-27 04:18:57 -0400

    update mason-lspconfig setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -118,15 +118,6 @@ require("lazy").setup({
       require("mason").setup()
       require("mason-lspconfig").setup()
 
-      require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {}
-        end,
-      }
-
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
         callback = function(event)
